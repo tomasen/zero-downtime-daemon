582f2236004b87f9d7fadb1fcf91744846c4c206
Tomasen <tomasen@gmail.com>
2013-06-05 02:42:32 +0800

    ????????pid?????

--- main.go
+++ main.go
@@ -6,8 +6,15 @@ import (
 	"flag"
   "os"
   "fmt"
+  "io"
   "os/signal"
+  "io/ioutil"
+  "path/filepath"
   "syscall"
+  "strconv"
+  "crypto/sha1"
+  
+  //"./daemon"
 )
 
 // configuration
@@ -26,6 +33,34 @@ func usage() {
   flag.PrintDefaults()
 }
 
+func readStringFromFile(filepath string) (string, error) {
+  contents, err := ioutil.ReadFile(filepath)
+  return string(contents), err
+}
+
+func writeStringToFile (filepath string, contents string) error {
+  return ioutil.WriteFile(filepath, []byte(contents), 0x777)
+}
+
+func getPidByConf (confPath string, prefix string) (int, error) {
+  
+  confPath,err := filepath.Abs(confPath)
+  if (err != nil) {
+    return 0, err
+  }
+  
+  hashSha1 := sha1.New()
+  io.WriteString(hashSha1, confPath)
+  pidFilepath := filepath.Join(os.TempDir(), fmt.Sprintf("%v_%x.pid", prefix, hashSha1.Sum(nil)))
+  
+  pidString, err := readStringFromFile(pidFilepath)
+  if (err != nil) {
+    return 0, err
+  }
+  
+  return strconv.Atoi(pidString)
+}
+
 func main() {
   // parse arguments
   flag.Parse()
@@ -38,12 +73,36 @@ func main() {
   }
 
   // find master process id by conf
-
+  pid,err := getPidByConf(*optConfigFile, "gozerodown")
+  if (err != nil) {
+    pid = 0 
+  }
+  
   // -s send signal to the process that has same config
   switch (*optSendSignal) {
-    case "stop":  
+    case "stop": 
+    if (pid != 0) {
+      p,err := os.FindProcess(pid)
+      if (err == nil) {
+        p.Signal(syscall.SIGTERM)
+        // wait it end
+        os.Exit(0)
+      }
+    }
+    os.Exit(0)
+    
     case "start","":
+      // start daemon
     case "reopen","reload":
+      if (pid != 0) {
+        p,err := os.FindProcess(pid)
+        if (err == nil) {
+          p.Signal(syscall.SIGTERM)
+          // wait it end
+          // start daemon
+        }
+      }
+      os.Exit(0)
   }
 
   // handle signals
@@ -58,8 +117,10 @@ func main() {
     fmt.Println("Got signal:", s)
     switch (s) {
       case syscall.SIGHUP, syscall.SIGUSR2:
+        // upgrade, reopen
       case syscall.SIGTERM, syscall.SIGINT:
+        // quit
+        os.Exit(0)
     }
   }
-  os.Exit(3)
 }
