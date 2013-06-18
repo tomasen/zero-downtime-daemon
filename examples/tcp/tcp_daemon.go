3bb8deb58c2f04065c6840f03390b1745a2e2a17
Reck Hou <reckhou@gmail.com>
2013-06-18 13:25:28 +0800

    Update: TCP Example

--- examples/tcp/tcp_daemon.go
+++ examples/tcp/tcp_daemon.go
@@ -12,6 +12,7 @@ const (
   TCP_MAX_TRANSMISSION_BYTES = 2 * 1024 // 2KB
   TCP_CONNECTION_TIMEOUT = 12 * time.Hour
 )
+
 func serveTCP(conn gozd.Conn) {
   fmt.Println("Caller serveTCP!")
   conn.SetDeadline(time.Now().Add(TCP_CONNECTION_TIMEOUT))
@@ -33,11 +34,15 @@ func serveTCP(conn gozd.Conn) {
 }
 
 func main() {
-  daemonChan := gozd.Daemonize() // returns channel that connects with daemon
+  daemonChan, isSucceed := gozd.Daemonize() // returns channel that connects with daemon
+  if !isSucceed {
+    os.Exit(1)
+  }
+
   err := gozd.RegistHandler("Group0", "serveTCP", serveTCP) // regist your own handle function, parameters MUST contain a "gozd.Conn" type.
   if err != nil {
-    fmt.Println(err.Error())
-    return
+    gozd.LogErr(err.Error())
+    os.Exit(1)
   }
   waitTillFinish(daemonChan) // wait till daemon send a exit signal
 }
