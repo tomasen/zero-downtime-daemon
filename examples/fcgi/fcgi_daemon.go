0b847127d775011974663a3f215ebf15e31e5d22
Tomasen <tomasen@gmail.com>
2013-08-13 13:28:37 +0800

    prototype for fcgi daemon

--- examples/fcgi/fcgi_daemon.go
+++ examples/fcgi/fcgi_daemon.go
@@ -1,20 +1,41 @@
 package main
 
 import (
- "net"
- "net/http"
- "net/http/fcgi"
+  //"bitbucket.org/PinIdea/go-zero-downtime-daemon"
+  "../../"
+  "net"
+  "net/http"
+  "net/http/fcgi"
 )
 
 type FastCGIServer struct{}
 
 func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
- resp.Write([]byte("<h1>Hello, 世界</h1>\n<p>Behold my Go web app.</p>"))
+  
+  switch{
+    case strings.HasPrefix(strings.ToLower(req.RequestURI), "/search"):
+      shooter.HandleSearch(resp, req)
+  }
+  resp.Write([]byte("<p>unable to handler the request</p>"))
 }
 
 func main() {
-  // Demonlize this
- listener, _ := net.Listen("unix", "/tmp/go.socket")
- srv := new(FastCGIServer)
- fcgi.Serve(listener, srv)
+  daemonChan, isSucceed := gozd.Daemonize() // returns channel that connects with daemon
+  if !isSucceed {
+    os.Exit(1)
+  }
+  
+  err := gozd.RegistHandler("Group0", "ServeFCGI", serveHTTP) // regist your own handle function, parameters MUST contain a "gozd.Conn" type.
+  if err != nil {
+    gozd.LogErr(err.Error())
+    os.Exit(1)
+  }
+  
+  waitTillFinish(daemonChan) // wait till daemon send a exit signal
+}
+
+func waitTillFinish(daemonChan chan int) {
+  code := <- daemonChan
+  gozd.Log("Exit tcp_daemon.")
+  os.Exit(code)
 }
